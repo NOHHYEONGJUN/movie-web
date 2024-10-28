10c7a35893df16bae32182f8dd3a521b0b281f65
NOHYEONGJUN <n7804@naver.com>
2024-10-28 18:05:08 +0900

    fix : MainPage Route 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/netflix" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
